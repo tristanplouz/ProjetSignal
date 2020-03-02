8f17ea8095a1208437ca38816c69aae4a1b0c0c1
skytro10 <florian.pouthier@laposte.net>
2020-03-02 18:48:35 +0100

    Mise en forme et commentaires

--- main.m
+++ main.m
@@ -1,40 +1,42 @@
-clear
+clear all;
 close all;
-clf;
+
+% Import libraries
+graphics_toolkit gnuplot
 pkg load signal
 pkg load image
 pkg load geometry
+
+% Image read
 B=imread("motif.png");
 mot=imread("mot.png");
-##c=imread("c.png");
-##mot2=imread("mot3.png");
-##q=imread("q.png");
-B=rgb2gray(B);
-B=im2double(B);
-##q=rgb2gray(q);
-##q=im2double(q);
+
+% Transform to gray scale
+B=rgb2gray(B);  
 mot=rgb2gray(mot);
+
+% Transform to double data
+B=im2double(B); 
 mot=im2double(mot);
-##c=rgb2gray(c);
-##c=im2double(c);
-####mot2=rgb2gray(mot2);
-####mot2=im2double(mot2);
+
 B=B-mean(mean(B));
 mot=mot-mean(mean(mot));
-%mot2-mot2-mean(mean(mot2));
-%ech=mot2(218:250,100:130);
-##
-saveas(imshow(B),"Rapports/illus/motifm0.png");
-saveas(imshow(mot),"Rapports/illus/motm0.png");
+
+% Save gray scale images
+% saveas(imshow(B),   "Rapports/illus/motifm0.png");
+% saveas(imshow(mot), "Rapports/illus/motm0.png");
+
 ech=B;
 figure(1)
 imshow(mot)
-figure
+figure(2)
 imshow(B)
+
 d=normxcorr2(ech,mot);
-figure
+figure(3)
 colormap("jet");
 shading("flat");
+
 saveas(surf(d,"edgecolor","none"),"Rapports/illus/cor.png");
 view(0,-90)
 V=surf(d,"edgecolor","none")
