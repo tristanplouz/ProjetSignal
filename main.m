471f576ac7d2d8c05810e3872c7ab09e6a63e9d9
Tristan <tristanplouz@gmail.com>
2020-03-24 11:47:58 +0100

    Organisation du code en fonction

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
-clear all;
-close all;
+%clear all;
+%close all;
 
 % Import libraries
 %graphics_toolkit gnuplot
@@ -7,20 +7,41 @@ pkg load signal
 pkg load image
 pkg load geometry
 
+%Function definition
+function res=findMax(d)
+  res=[];
+  ja=length(d(1,:))-1;
+  ia=length(d(:,1))-1;
+  for i=1:ia
+    for j=1:ja
+      if d(i,j)>0.9
+        res=[res; [i,j]];
+      endif
+    endfor
+  endfor
+endfunction
+
+
+function showLoc (res,ech)
+  figure(1)
+  hold on;
+  for i=1:length(res)
+    drawCircle(res(i,2)-length(ech(1,:))/2, res(i,1)-length(ech(:,1))/2, 10)
+  endfor
+endfunction
+
+function res=picPrep(pic)
+  pic=rgb2gray(pic); % Transform to gray scale
+  pic=im2double(pic);% Transform to double data
+  res=pic-mean(mean(pic));
+endfunction
 % Image read
-B=imread("motif3.png");
+B=imread("R.png");
 mot=imread("mot.png");
+%mot=imread("../photomotif.jpg");
 
-% Transform to gray scale
-B=rgb2gray(B);  
-mot=rgb2gray(mot);
-
-% Transform to double data
-B=im2double(B); 
-mot=im2double(mot);
-
-B=B-mean(mean(B));
-mot=mot-mean(mean(mot));
+B=picPrep(B);
+mot=picPrep(mot);
 
 % Save gray scale images
 % saveas(imshow(B),   "Rapports/illus/motifm0.png");
@@ -37,23 +58,9 @@ colormap("jet");
 shading("flat");
 
 surf(d,"edgecolor","none")
-saveas(figure(3),"Rapports/illus/cor.png");
+%saveas(figure(3),"Rapports/illus/cor.png");
 view(0,-90)
-saveas(figure(3),"Rapports/illus/cor1.png");
-figure(1)
-hold on;
-res=[];
-ja=length(d(1,:))-1;
-ia=length(d(:,1))-1;
-for i=1:ia
-  for j=1:ja
-    if d(i,j)>0.9
-      res=[res; [i,j]];
-    endif
-  endfor
-endfor
+%saveas(figure(3),"Rapports/illus/cor1.png");
+showLoc(findMax(d),ech);
 
-for i=1:length(res)
-  drawCircle(res(i,2)-length(ech(1,:))/2, res(i,1)-length(ech(:,1))/2, 10)
-endfor
-saveas(figure(1),"Rapports/illus/motiflocal3.png")
+%saveas(figure(1),"Rapports/illus/motiflocal3.png")
