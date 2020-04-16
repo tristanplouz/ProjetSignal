7ff96d9657bd1cfb47966957d22246cfd3a5a7c0
Tristan <tristanplouz@gmail.com>
2020-04-16 18:35:27 +0200

    Modif

--- main.m
+++ main.m
@@ -15,21 +15,26 @@ function res=picPrep(pic)
 endfunction
 
 data=glob("alphabet/*.png");
-mot=imread("mot.png");
+mot=imread("lorem.png");
 mot=picPrep(mot);
 %figure(1)
 %imshow(mot)
+dx=dy=0;
+for i=1:length(data)-1
+  tmp=imread(data{i});
+  tmp=picPrep(tmp);
+  dy+=length(tmp(:,1));
+  dx+=length(tmp(1,:));
+endfor
+dy=109;
+dx/=i;
+dx+=11;
 
-tmp=imread(data{1});
-tmp=rgb2gray(tmp);
-dy=length(tmp(:,1));
-dx=length(tmp(1,:));
+nb_carac_line=round(length(mot(1,:))/dx)
+nb_carac_col =round(length(mot(:,1))/dy)
 
-nb_carac_line=floor(length(mot(1,:))/dx);
-nb_carac_col =floor(length(mot(:,1))/dy);
-
-dx_search = floor(length(mot(1,:))/nb_carac_line)
-dy_search = floor(length(mot(:,1))/nb_carac_col)
+dx_search = round(length(mot(1,:))/nb_carac_line);
+dy_search = round(length(mot(:,1))/nb_carac_col);
 
 mot_pred = "";
 
@@ -50,10 +55,11 @@ for j=1:nb_carac_col
       imshow(ech);
       %Display mot
       figure(4); 
-      imshow(mot(dy_search*(j-1)+1:dy_search*(j-1)+dy_search+1,dx_search*(i-1)+1:dx_search*(i-1)+dx_search+1));
-      
-      % Intercorrelation
-      d=normxcorr2(ech,mot(dy_search*(j-1)+1:dy_search*(j-1)+dy_search+1,dx_search*(i-1)+1:dx_search*(i-1)+dx_search+1));
+      imshow(mot(dy_search*(j-1)+1:dy_search*(j-1)+dy_search,dx_search*(i-1)+1:dx_search*(i-1)+dx_search)); 
+       
+      % Intercorrelation 
+      d=normxcorr2(ech,mot(dy_search*(j-1)+1:dy_search*(j-1)+dy_search,dx_search*(i-1)+1:dx_search*(i-1)+dx_search)); 
+      %figure(3);
       %figure(3);
       %colormap("jet");
       %surf(d,"edgecolor","none")
@@ -62,19 +68,21 @@ for j=1:nb_carac_col
       max_local=max(max(d));
       if max_local > max_global
         max_global = max_local
-         if max_global < 0.85
-              predict = "\ "
+        if max_global < 0.85
+           predict = "\ "
         else
         predict = l
-      endif
-        
+        endif
+        if max_global > 0.9
+          break
+        endif
       endif
      
     endfor
-    mot_pred = strcat(mot_pred, predict)
+    mot_pred = cstrcat(mot_pred, predict)
     mot_pred
   endfor
-  mot_pred = strcat(mot_pred, "\n")
+  mot_pred = cstrcat(mot_pred, "\n")
   mot_pred
 endfor
 
